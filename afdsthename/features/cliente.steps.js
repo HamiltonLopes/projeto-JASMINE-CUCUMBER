183e8a60040695299469675052be33cc38aefe09
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-04-29 23:16:40 -0300

    funcionooouuu

--- afdsthename/features/cliente.steps.js
+++ afdsthename/features/cliente.steps.js
@@ -2,11 +2,11 @@
  const assert = require('assert');
  const {Given,When,Then} = require('cucumber');
  const path = require('path');
- const {ServiceBuilder} = require ('selenium-webdriver/chrome');
- const {Builder, By, Key, untill} = require('selenium-webdriver');
- const pathDriver = "C:/Users/FINANCEIRO/Desktop/Hiring/projeto-JASMINE-CUCUMBER/afdsthename/driver/chromedriver.exe";
+ const {ServiceBuilder} = require ('selenium-webdriver/edge');
+ const {Builder, By, Key, untill, Browser} = require('selenium-webdriver');
+ const pathDriver = "C:/Users/Hamilton/Desktop/teste/HIRINGCODERS/projeto-JASMINE-CUCUMBER/afdsthename/driver/msedgedriver.exe";
  const serviceBuilder = new ServiceBuilder(pathDriver);
- const driver = new Builder().forBrowser('chrome').setChromeService(serviceBuilder).build();
+ const driver = new Builder().forBrowser(Browser.EDGE).setEdgeService(serviceBuilder).build();
 
  Given('I have {string} clients in my database', function (string) {
     console.log("inseri os dados na base de dados!!");
@@ -14,13 +14,13 @@
 
 
   When('I access the home webpage', async function () {
-    await driver.get('https://localhost:3000');
-    await driver.sleep('1000');
+    await driver.get('http://localhost:3000');
+    await driver.sleep(1000);
 });
 
 
   Then('see the list of {string} itens', async function (string) {
-    let qtd = await driver.findElement(By.tagName('tr')); 
+    let qtd = await driver.findElements(By.css('tbody tr')); 
     assert.equal(qtd.length, parseInt(string));
     await driver.quit();
 });
