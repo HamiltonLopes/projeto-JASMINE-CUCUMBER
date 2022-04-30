8ba7ce5b76b5e4dab5147b39a614ac37220acb9a
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-04-30 00:11:07 -0300

    Jest e cypress

--- afdsthename/spec/models/cliente.spec.js
+++ afdsthename/spec/models/cliente.spec.js
@@ -4,19 +4,19 @@ describe('Clientes', () =>{
     
     })
 
-    it('Teste 1', () => {
+    test('Teste 1', () => {
         let cliente = Cliente.primeiro();
         expect(cliente.id).not.toBeUndefined();
         expect(cliente.nome).not.toBeUndefined();
         expect(cliente.telefone).not.toBeUndefined();
     });
 
-    it('Teste 2', ()=>{
+    test('Teste 2', ()=>{
         let clientes = Cliente.todos();
         expect(clientes.length).toEqual(10);
     });
 
-    it('Teste 3', ()=>{
+    test('Teste 3', ()=>{
         let cliente = Cliente.primeiro();
         cliente.nome = 'danilo';
         expect(cliente.nomeUperCase()).toEqual('DANILO');
