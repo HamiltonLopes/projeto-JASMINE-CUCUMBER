8d00d5846b8ee242ad9d1a491b8673b559419d62
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-04-30 00:11:07 -0300

    new one

--- afdsthename/spec/models/cliente.spec.js
+++ afdsthename/spec/models/cliente.spec.js
@@ -4,19 +4,19 @@ describe('Clientes', () =>{
     
     })
 
-    test('Teste 1', () => {
+    it('Teste 1', () => {
         let cliente = Cliente.primeiro();
         expect(cliente.id).not.toBeUndefined();
         expect(cliente.nome).not.toBeUndefined();
         expect(cliente.telefone).not.toBeUndefined();
     });
 
-    test('Teste 2', ()=>{
+    it('Teste 2', ()=>{
         let clientes = Cliente.todos();
         expect(clientes.length).toEqual(10);
     });
 
-    test('Teste 3', ()=>{
+    it('Teste 3', ()=>{
         let cliente = Cliente.primeiro();
         cliente.nome = 'danilo';
         expect(cliente.nomeUperCase()).toEqual('DANILO');
